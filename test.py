36a1777f24896b5657104dea3bfd1cfe807faf06
Rudya93 <rudyaa93@gmail.com>
2018-03-29 11:15:46 +0300

    upd

--- test.py
+++ test.py
@@ -1,10 +1,21 @@
-num_array = list()
+a = []
+i=0
+for i in range(int(3)):
+    b = input("input matrix 1: ")
+    a.append(b)
+print (a)
+#num_array[:, i]
+
+  #a = [1, 2, 3]
+  #print (a[: 3])
+#num_array = list()
 #num = input("Enter how many elements you want:")
 #print 'Enter numbers in array: '
-for i in range(int(3)):
-    input ("input matrix 1 line: " ) a
+#for i in range(int(3)):
+ #   input ("input matrix 1 line: " ) a
 
-num_array[:, i]
+#num_array[:, i]
+
 
    # n = input("num :")
     #num_array.append(int(n))
