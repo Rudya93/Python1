7546e015ceb25c6a6fa9961c840b5cf590a3acff
Rudya93 <rudyaa93@gmail.com>
2018-03-29 16:56:04 +0300

    8,6

--- 6.py
+++ 6.py
@@ -1,4 +1,21 @@
 #def caesarCipher():
+import string
+#print (string.ascii_uppercase)
+for c in string.ascii_letters:
+    print(c)
+
+s = 'fooУБРАТЬbarОТСЮДАbazНЕЛАТИНСКОЕ'
+s2 = ''.join(c for c in s if c in string.ascii_letters)
+print(s2)
+#chr(65)
+#'A'
+#chr(122)
+#'z'
+#print(chr(128522))
+
+
+
+
 print('Hello'.replace('l', 'L'))
 # вернёт 'HeLLo'
 
--- 8.py
+++ 8.py
@@ -1,15 +1,16 @@
 import random
-c=[]
+c =[]
 print("Welcome to Game")
 i = 1
 for i in range(int(3)):
     print ('input number:' , i)
     b = input()
+    b = (int(b))
     c.append(b)
-print (c)
+print  (c)
 
-a=[]
-a=([random.randint(0, 10) for i in range(3)])
+a =[]
+a =([random.randint(0, 100) for i in range(3)])
 print(a)
 
 #for i in (int(3)): if
